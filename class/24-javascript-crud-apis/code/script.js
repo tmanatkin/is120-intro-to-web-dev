0878f1e33ca01f68fa586740db92643df7090720
tanneratkin <tmanatkin@gmail.com>
2026-04-08 13:13:28 -0600

    error handling for supabase

--- class/24-javascript-crud-apis/code/script.js
+++ class/24-javascript-crud-apis/code/script.js
@@ -2,20 +2,32 @@ const db = supabase.createClient("https://insert-url-here.supabase.co", "sb_publ
 
 async function getAll() {
   let { data, error } = await db.from("table_1").select("*");
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
   console.log(data);
 }
 
 async function createRow() {
-  const { data, error } = await db
+  const { error } = await db
     .from("table_1")
     .insert([{ column_1: "new Value!" }])
     .select();
-  console.log(data);
+
+  if (error) {
+    console.error(error);
+  }
 }
 
 async function deleteRow() {
   const { error } = await db.from("table_1").delete().eq("id", "2");
-  console.log(error);
+
+  if (error) {
+    console.error(error);
+  }
 }
 
 async function editRow() {
@@ -24,5 +36,8 @@ async function editRow() {
     .update({ column_1: "brand new value" })
     .eq("column_1", "new Value!")
     .select();
-  console.log(data);
+
+  if (error) {
+    console.error(error);
+  }
 }
